9f0cbd1437dddcff91a9d8cb762c87b477083148
daido1976 <daido1976@gmail.com>
2022-11-01 15:56:44 +0900

    Impl FizzBuzz

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
-
-def msg = "I was compiled by Scala 3. :)"
+@main
+def main(): Unit = {
+  for (i <- 1 to 100) {
+    val x = (i % 3, i % 5) match
+      case (0, 0) => "FizzBuzz"
+      case (0, _) => "Fizz"
+      case (_, 0) => "Buzz"
+      case _      => i
+    println(x)
+  }
+}
