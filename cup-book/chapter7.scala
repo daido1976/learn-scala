e61e35d5ca9b9099ccb70103c4d60a137c195efa
daido1976 <daido1976@gmail.com>
2022-11-04 18:16:39 +0900

    Add test & Improve chapter7

--- cup-book/chapter7.scala
+++ cup-book/chapter7.scala
@@ -1,15 +1,22 @@
 package chapter7
 
+// NOTE: Optimal values depend on the output display size.
+val SectionSize = 4
+val Capacity = 30
+
 private def makeRowSeq(row: Int, end: Int): IndexedSeq[String] =
   for (column <- 1 to end) yield {
     val product = (row * column).toString()
-    val padding = " " * (4 - product.length())
+    val padding = " " * (SectionSize - product.length())
     padding + product
   }
 
 private def makeRow(row: Int, end: Int): String = makeRowSeq(row, end).mkString
 
-def multiTable(end: Int) = {
+def multiTable(end: Int): String = {
+  // TODO: Return Either to prevent an Exception.
+  require(end <= Capacity)
+
   val tableSeq = for (row <- 1 to end) yield makeRow(row, end)
   tableSeq.mkString("\n")
 }
