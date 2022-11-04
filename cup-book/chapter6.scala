7292fffea1d7ee1aca3fee4f5a04b9c6b795992a
daido1976 <daido1976@gmail.com>
2022-11-04 15:17:57 +0900

    Impl Rational class of cup-book

---
+++ cup-book/chapter6.scala
@@ -0,0 +1,62 @@
+package chapter6
+
+class Rational(n: Int, d: Int) {
+  require(d != 0)
+
+  private val g = gcd(n.abs, d.abs)
+  val numerator = n / g
+  val denominator = d / g
+
+  def this(n: Int) = this(n, 1)
+
+  def +(that: Rational): Rational = new Rational(
+    numerator * that.denominator + that.numerator * denominator,
+    denominator * that.denominator
+  )
+
+  def +(i: Int): Rational = new Rational(
+    numerator + i * denominator,
+    denominator
+  )
+
+  def -(that: Rational): Rational = new Rational(
+    numerator * that.denominator - that.numerator * denominator,
+    denominator * that.denominator
+  )
+
+  def -(i: Int): Rational = new Rational(
+    numerator - i * denominator,
+    denominator
+  )
+
+  def *(that: Rational): Rational = new Rational(
+    numerator * that.numerator,
+    denominator * that.denominator
+  )
+
+  def *(i: Int): Rational = new Rational(
+    numerator * i,
+    denominator
+  )
+
+  def /(that: Rational): Rational = new Rational(
+    numerator * that.denominator,
+    denominator * that.numerator
+  )
+
+  def /(i: Int): Rational = new Rational(
+    numerator,
+    denominator * i
+  )
+
+  override def toString(): String = s"$numerator/$denominator"
+
+  // Derive the greatest common divisor with Euclidean Algorithm.
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+}
+
+@main
+def main(args: String*): Unit = {
+  implicit def intToRational(x: Int): Rational = new Rational(x)
+  println(2 * new Rational(2, 3))
+}
---
+++ cup-book/chapter6.test.scala
@@ -0,0 +1,18 @@
+//> using lib "org.scalameta::munit::0.7.27"
+package chapter6
+
+class MyTests extends munit.FunSuite {
+  test("Rational") {
+    val x = new Rational(2, 3)
+    val y = new Rational(1, 2)
+    assertEquals((x.toString(), y.toString()), ("2/3", "1/2"))
+    assertEquals((x + y).toString(), "7/6")
+    assertEquals((x + 2).toString(), "8/3")
+    assertEquals((x - y).toString(), "1/6")
+    assertEquals((x - 2).toString(), "-4/3")
+    assertEquals((x * y).toString(), "1/3")
+    assertEquals((x * 2).toString(), "4/3")
+    assertEquals((x / y).toString(), "4/3")
+    assertEquals((x / 2).toString(), "1/3")
+  }
+}
